1bcb96dc8380c0c14d702faacdac06c197fe3c10
BillRob <theman@fdrsucks.com>
2013-03-19 01:42:09 -0500

    Added beside configuration stack to hold all the loading and timeout configuration for a Type such as cConsumer or cUser.

---
+++ BlendedCache.Framework.Tests/TDataLoaderMock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlendedCache.Tests
+{
+	/// <summary>
+	/// Mock interface that is starting out usable everywhere the TDataLoader generic type is used.
+	/// </summary>
+	public interface TDataLoaderMock
+	{
+	}
+}
---
+++ BlendedCache.Framework/Configuration/IBlendedCacheConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlendedCache
+{
+	/// <summary>
+	/// Interface representing the configuration information for the defined TDataLoader such 
+	/// as type loaders and timeouts.  These are the Types that are return from cache, not the types loaded
+	/// inside the TDataLoader methods.
+	/// </summary>
+	public interface IBlendedCacheConfiguration
+	{
+		/// <summary>
+		/// Will get the type configuration information for the requested type.  If the EnforceAllLoadedTypesAreDefind flag
+		/// is set and the type hasn't been defined, this method will throw a BlendedCacheConfigurationException.
+		/// </summary>
+		/// <param name="type">The type the configuration object is being requested for.</param>
+		/// <returns></returns>
+		ITypeConfiguration GetTypeConfiguration(Type type);
+
+		/// <summary>
+		/// Contains the default cache timeout structure when a TypeConfiguration is not defined.
+		/// </summary>
+		ICacheTimeout DefaultCacheTimeout { get; }
+	}
+}
